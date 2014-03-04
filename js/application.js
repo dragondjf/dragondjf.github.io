139dcc5af9dadcb13b6effc72c07961394e32f96
djf <dragondjf@gmail.com>
2014-03-04 23:48:23 +0800

    fix

--- js/application.js
+++ js/application.js
@@ -47,7 +47,7 @@ $('div .shared-by').click(function(event) {
     // flag = $.inArray(current, all);
     // all.splice(current, 1);
     // all.hide();
-    current.fadeIn('fast');
+    current.fadeToggle('fast');
 });
 
 $(document).ready(function() {
